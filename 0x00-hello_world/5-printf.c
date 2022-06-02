41e030a3bd305db5a42f4303e04f877f484f62d5
HEGHELON <humphreyeghelonohor@yahoo.com>
2022-06-02 10:30:34 -0700

    A c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - It prints a function
  *
- * * main - print the string in the put function
+ * Description: It print a function
  *
- * *
+ * Return: 0
  *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle"
- *
- * * Return: 0
- *
- * */
-
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
