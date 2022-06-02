076938d599fe67e260c7d4201664e2b5035ce971
HEGHELON <humphreyeghelonohor@yahoo.com>
2022-06-02 00:48:53 -0700

    a c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,19 @@
 
 
 
-/*
- *  main - print the string in the put function
-*/
+/**
+ *
+ * * main - print the string in the put function
+ *
+ * *
+ *
+ * * Description: using the main function
+ *
+ * * this program prints "Programming is like building a multilingual puzzle"
+ *
+ *  * Return: 0
+ *
+ * */
 
 int main(void)
 
@@ -12,6 +22,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-		return (0);
+			return (0);
 
 }
