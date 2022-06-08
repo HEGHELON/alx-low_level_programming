339ee2a67b3825d6c1229179026113add590231b
HEGHELON <humphreyeghelonohor@yahoo.com>
2022-06-08 03:36:11 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,16 +5,17 @@
  * @n: integer to check
  *
  * Return: n
- */
+*/
 
 int _abs(int n)
 {
-    if (n >= 0)
-        return (n);
-
-    else
-    {
-        n *= -1;
-        return (n);
-    }
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n *= -1;
+		return (n);
+	}
 }
