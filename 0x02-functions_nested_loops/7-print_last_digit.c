fc2f791aa1ab58212ce1c71d3b82dafab0b39d12
HEGHELON <humphreyeghelonohor@yahoo.com>
2022-06-08 04:13:58 -0700

    lasr digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,16 +5,16 @@
  * @n: integer to modify
  *
  * Return: l
- */
+*/
 
 int print_last_digit(int n)
 {
-    int l, p;
+	int l, p;
 
-    l = n % 10;
-    if (l < 0)
-        l = -l;
-    p = '0' + l;
-    _putchar(p);
-    return (l);
+	l = n % 10;
+	if (l < 0)
+		l = -l;
+	p = '0' + l;
+	_putchar(p);
+	return (l);
 }
