1d6d2d8385784f9215bb0ca55cf72b41d574b07f
HEGHELON <humphreyeghelonohor@yahoo.com>
2022-06-24 17:03:51 -0700

    add

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 
 /**
  * _strlen - returns length of a string
@@ -10,9 +10,5 @@
 */
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while( *s++ )
-		len++;
-	return (len);
+	return (strlen(s));
 }
